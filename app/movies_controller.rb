32e5ee2763e2911463b17a5a702a085207105a35
Haley DeKeyser <hcd09@live.com>
2019-06-18 20:46:05 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -48,7 +48,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find(:title, :release_date, :director)
+  Movie.find_by
 end
 
 def can_find_using_where_clause_and_be_sorted
