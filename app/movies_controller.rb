03626f8a7aa2ffcd979183e2eecf85306fbb81f4
Haley DeKeyser <hcd09@live.com>
2019-06-18 19:48:45 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -48,7 +48,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find(:conditions)
+  Movie.all(options)
 end
 
 def can_find_using_where_clause_and_be_sorted
