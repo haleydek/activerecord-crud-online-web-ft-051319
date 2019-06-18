8d6763c226679b02731b2515d7ddbc9969d45345
Haley DeKeyser <hcd09@live.com>
2019-06-18 18:35:17 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_in_a_block(args = {title: 'Home Alone', release_date: 1990})
   # release_date == 1990
   
   Movie.create do |m|
-    m = Movie.new(title, release_date)
+    
   end
 end
 
